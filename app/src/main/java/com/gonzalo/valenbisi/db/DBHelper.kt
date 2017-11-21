164e800ccdf64268a271a4133b72ce9c097300fa
gon250 <zalo250@gmail.com>
2017-11-21 19:40:05 +0100

    Added db.

---
+++ app/src/main/java/com/gonzalo/valenbisi/db/DBHelper.kt
@@ -0,0 +1,37 @@
+package com.gonzalo.valenbisi.db
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import org.jetbrains.anko.db.*
+
+
+class DBHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "Valenbisi", null, 1) {
+
+    val stationDb = "Station"
+
+    companion object {
+        private var instance: DBHelper? = null
+
+        fun getInstance(ctx: Context): DBHelper? = if (instance == null) {
+            instance = DBHelper(ctx)
+            instance
+        } else {
+            instance
+        }
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        val id = "id" to INTEGER + PRIMARY_KEY
+        val stationId = "stationId" to TEXT
+        val name = "name" to TEXT
+        val status = "" to REAL
+        db?.createTable(stationDb, true, id, stationId, name, status)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, old: Int, newV: Int) {
+        db?.dropTable(stationDb, true)
+    }
+
+    val Context.database: DBHelper?
+        get() = getInstance(applicationContext)
+}
