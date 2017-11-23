6d8039bcc136cc3fee42c6bc11e7925320949a3b
gon250 <zalo250@gmail.com>
2017-11-23 19:03:05 +0100

    Removed Anko sqlite

--- app/src/main/java/com/gonzalo/valenbisi/adapters/AdapterStation.kt
+++ app/src/main/java/com/gonzalo/valenbisi/adapters/AdapterStation.kt
@@ -5,11 +5,8 @@ import android.view.View
 import android.view.ViewGroup
 import com.gonzalo.valenbisi.models.Station
 import com.gonzalo.valenbisi.R
-import com.gonzalo.valenbisi.db.DBHelper
 import com.gonzalo.valenbisi.helpers.inflate
 import kotlinx.android.synthetic.main.item_station.view.*
-import org.jetbrains.anko.db.insert
-
 class AdapterStation(val data: List<Station?>?) : RecyclerView.Adapter<AdapterStation.Holder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): Holder =
@@ -29,26 +26,12 @@ class AdapterStation(val data: List<Station?>?) : RecyclerView.Adapter<AdapterSt
         fun bindView(station: Station?) {
             station.let {
                 with(it) {
-                    val stId = it?.stationId
-                    val stName = it?.name
-                    val stStatus = "1" //TODO: Implement Status
-
                     itemView.txtStationName.text = it?.stationId
                     itemView.txtStationBikes.text = it?.freeBikes.toString()
                     itemView.txtStationSlots.text = it?.slots.toString()
                     itemView.txtStationDate.text = it?.lastUpdate.toString()
                     itemView.txtStationDistance.text = "0.00 m" //TODO: Calculate Distance
 
-                    itemView.favBtn.setOnClickListener {
-                        val db = DBHelper.getInstance(itemView.context)
-                        db?.use {
-                            val stationId = "stationId" to stId
-                            val stationName = "name" to stName
-                            val stationStatus = "status" to stStatus
-                            insert("Station",  stationId, stationName, stationStatus)
-                        }
-                        //TODO: Change image icon.
-                    }
                 }
             }
         }
---
+++ app/src/main/java/com/gonzalo/valenbisi/repository/StationRepository.kt
@@ -0,0 +1,11 @@
+package com.gonzalo.valenbisi.repository
+
+
+class StationRepository {
+
+    fun upfateStation() {
+
+    }
+
+
+}
